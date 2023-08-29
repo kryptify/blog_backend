79f222f6e1884ac289622b34d7dc85dc59360ab3
Fedorov100 <truesight9705@gmail.com>
2023-08-30 02:08:57 +0900

    revise

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 const start = async () => {
     app.listen(PORT, () => {
         /* eslint-disable no-console */
-        log.info(`Listening: http://blog-backend-blue.vercel.app:${PORT}`);
+        log.info(`Listening: https://blog-backend-wfo2.onrender.com:${PORT}`);
         /* eslint-enable no-console */
     });
     await tools.init();
